284685fbdb7f0dfac035a63b1cda22df04698367
SECTION <akasection@outlook.com>
2021-12-07 19:11:40 +0700

    deploy

---
+++ precache-manifest.8d6f273b3b635a3a4c525eeacd822454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcd6e844e1297792220",
+    "url": "/css/app.9aa1e0d6.css"
+  },
+  {
+    "revision": "68cf862a6dc78a6464beca6f526286bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dcd6e844e1297792220",
+    "url": "/js/app.00695cd1.js"
+  },
+  {
+    "revision": "86d75a0151e9483a3b8c",
+    "url": "/js/chunk-vendors.e57bf25f.js"
+  },
+  {
+    "revision": "e53b7f242a9c9f16d8b822b35f01f619",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
